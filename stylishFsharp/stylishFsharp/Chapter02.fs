cbdf6ac1c85470afcd5bca9a6c1850076e31ac6c
Patrick Drechsler <patrick@pdrechsler.de>
2020-08-12 02:54:57 +0200

    ch02: exercise 2-3 - minimal test and impl (according to book)

--- stylishFsharp/stylishFsharp/Chapter02.fs
+++ stylishFsharp/stylishFsharp/Chapter02.fs
@@ -26,27 +26,58 @@ module MilesYards =
         ~~wholeMiles + (~~yards / 1760.)
 
     let value (MilesYards (wholeMiles, yards)) = wholeMiles, yards
+    
+module MilesChains =
+
+    let private (~~) = float
+
+    type MilesChains =
+        private MilesChains of wholeMiles : int * chains : int
+    
+    let fromMilesChains(wholeMiles : int, chains : int) =
+        if wholeMiles < 0 then
+            raise <| ArgumentOutOfRangeException("wholeMiles",
+                                                 "Must be >= 0")
+            
+        if chains < 0 || chains >= 80 then
+            raise <| ArgumentOutOfRangeException("chains",
+                                                 "Must be >= 0 and < 80")
+        MilesChains(wholeMiles, chains)
+        
+    let toDecimalMiles (MilesChains(wholeMiles, chains)) =
+        ~~wholeMiles + (~~chains / 80.)
 
 
 open Xunit
 open FsUnit.Xunit
 
 open MilesYards
+open MilesChains
 
 [<Fact>]
-let ``fromMilesPointYards split miles and yards`` () =
+let ``MilesYards fromMilesPointYards split miles and yards`` () =
     1.0880 |> fromMilesPointYards |> value |> should equal (1, 880)
 
 [<Fact>]
-let ``fromMilesPointYards throws when yards fraction too large`` () =
+let ``MilesYards fromMilesPointYards throws when yards fraction too large`` () =
     (fun () -> 1.1760 |> fromMilesPointYards |> ignore)
     |> should throw typeof<ArgumentOutOfRangeException>
 
 [<Fact>]
-let ``toDecimalMiles 1 mile and 880 yards is 1.5 miles`` () =
-    1.0880 |> fromMilesPointYards |> toDecimalMiles |> should equal 1.5
+let ``MilesYards toDecimalMiles 1 mile and 880 yards is 1.5 miles`` () =
+    1.0880 |> fromMilesPointYards |> MilesYards.toDecimalMiles |> should equal 1.5
 
 [<Fact>]
-let ``fromMilesPointYards throws when input is negative`` () =
+let ``MilesYards fromMilesPointYards throws when input is negative`` () =
     (fun () -> -1. |> fromMilesPointYards |> ignore)
     |> should throw typeof<ArgumentOutOfRangeException>
+
+
+[<Fact>]
+let ``MilesChains fromMilesChains throws when chain too large`` () =
+    (fun () -> (1, 80) |> fromMilesChains |> ignore)
+    |> should throw typeof<ArgumentOutOfRangeException>
+
+[<Fact>]
+let ``MilesChains fromMilesChains toDecimalValue 1 40 is 1.5 miles`` () =
+    (1, 40) |> fromMilesChains |> MilesChains.toDecimalMiles |> should equal 1.5
