ced49c2157f587d03407878d34d87d4b475f9f92
Patrick Drechsler <patrick@pdrechsler.de>
2020-12-18 20:25:20 +0100

    ch08: exercise 8-4

--- stylishFsharp/stylishFsharp.Tests/Chapter08Tests.fs
+++ stylishFsharp/stylishFsharp.Tests/Chapter08Tests.fs
@@ -1,5 +1,6 @@
 module Chapter08Tests
 
+open System.Drawing
 open Xunit
 open Swensen.Unquote
 open Chapter08
@@ -13,15 +14,35 @@ let ``Exercise 8-1: A simple class`` () =
 
 [<Fact>]
 let ``Exercise 8-2: Secondary Constructor`` () =
-    let grayScale = GrayScale(System.Drawing.Color.Brown)
+    let grayScale = GrayScale(Color.Brown)
     let actual = grayScale.Level
     let expected = 83uy
     test <@ actual = expected @>
 
 [<Fact>]
 let ``Exercise 8-3: Overrides ToString`` () =
-    let grayScale = GrayScale(System.Drawing.Color.Brown)
+    let grayScale = GrayScale(Color.Brown)
     let actual = grayScale.ToString()
     let expected = "GrayScale(83)"
     test <@ actual = expected @>
+
+[<Fact>]
+let ``Exercise 8-4: Equality 1/3 Orange is Orange`` () =
+    let orangeByName = GrayScale(Color.Orange)
+    let orangeByRGB = GrayScale(0xFFuy, 0xA5uy, 0x00uy)
+    test <@ orangeByName = orangeByRGB @>
+    
+[<Fact>]
+let ``Exercise 8-4: Equality 2/3 Orange is not Blue`` () =
+    let orange = GrayScale(Color.Orange)
+    let blue = GrayScale(Color.Blue)
+    test <@ orange <> blue @>
+
+[<Fact>]
+let ``Exercise 8-4: Equality 3/3 Comparing similar colors`` () =
+    let orange = GrayScale(0xFFuy, 0xA5uy, 0x00uy)
+    let otherColor = GrayScale(0xFFuy, 0xA5uy, 0x01uy) // <- Note: `0x01uy` != `0x00uy` 
+    
+    // This returns true, because we lose accuracy during rounding! 
+    test <@ orange = otherColor @>
     
--- stylishFsharp/stylishFsharp/Chapter08.fs
+++ stylishFsharp/stylishFsharp/Chapter08.fs
@@ -1,11 +1,34 @@
 module Chapter08
 
+open System
+open System.Drawing
+
 type GrayScale (r : byte, g : byte, b : byte) =
-    new (color : System.Drawing.Color) =
+
+    let level = (int r + int g + int b) / 3 |> byte
+    
+    let eq (that : GrayScale) =
+        level = that.Level
+    
+    new (color : Color) =
         GrayScale(color.R, color.G, color.B)
         
     member __.Level =
-        (int r + int g + int b) / 3 |> byte
+        level    
         
     override __.ToString () =
         sprintf "GrayScale(%i)" __.Level
+            
+    override this.GetHashCode () =
+        hash level
+
+    override __.Equals (thatObj) =
+        match thatObj with
+        | :? GrayScale as that ->
+            eq that
+        | _ ->
+            false
+            
+    interface IEquatable<GrayScale> with
+        member __.Equals(that : GrayScale) =
+            eq that
