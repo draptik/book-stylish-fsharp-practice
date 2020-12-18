fc3a71f7c1797f4d66fb3d4b74ea7138dbcca579
Patrick Drechsler <patrick@pdrechsler.de>
2020-12-18 22:40:33 +0100

    ch09: exercise 9-2

--- stylishFsharp/stylishFsharp.Tests/Chapter09Tests.fs
+++ stylishFsharp/stylishFsharp.Tests/Chapter09Tests.fs
@@ -16,3 +16,34 @@ let ``Exercise 9-1: Functions as arguments - 2/2 subtract without named function
     let actual' = applyAndPrint (-) 10 3 // <- shorter syntax alternative passing the operator into `applyAndPrint`
     test <@ actual = 7 @>
     test <@ actual' = 7 @>
+
+[<Fact>]
+let ``Exercise 9-2: Functions returning functions (with hidden state inside the function!) - 1/2 counter`` () =
+    let counterFcn1 = counter 0
+    let counterFcn2 = counter 100
+
+    test <@ counterFcn1() = 0 @>
+    test <@ counterFcn1() = 1 @>
+    test <@ counterFcn1() = 2 @>
+    test <@ counterFcn1() = 3 @>
+    
+    test <@ counterFcn2() = 100 @>
+    test <@ counterFcn2() = 101 @>
+    test <@ counterFcn2() = 102 @>
+    test <@ counterFcn2() = 103 @>
+    
+[<Fact>]
+let ``Exercise 9-2: Functions returning functions (with hidden state inside the function!) - 2/2 rangeCounter`` () =
+    let actualFcn = rangeCounter 3 6
+
+    test <@ actualFcn() = 3 @>
+    test <@ actualFcn() = 4 @>
+    test <@ actualFcn() = 5 @>
+    test <@ actualFcn() = 6 @>
+    test <@ actualFcn() = 3 @>
+    test <@ actualFcn() = 4 @>
+    test <@ actualFcn() = 5 @>
+
+[<Fact>]
+let ``Exercise 9-3`` () =
+    0
--- stylishFsharp/stylishFsharp/Chapter09.fs
+++ stylishFsharp/stylishFsharp/Chapter09.fs
@@ -6,3 +6,29 @@ let applyAndPrint f a b =
     let r = f a b
     printfn "%i" r
     r
+    
+let counter start =
+    (* Neat trick to hide state inside a function *) 
+    let mutable current = start
+    
+    fun () ->
+        let this = current
+        current <- current + 1
+        this
+
+(* This function returns a function that generates numbers
+in a circular pattern between a specified range *)
+let rangeCounter lowerBound upperBound =
+    (* Neat trick to hide state inside a function *)
+    let mutable current = lowerBound
+    
+    fun () ->
+        let this = current
+        let next = current + 1
+        current <-
+            if next > upperBound then
+                lowerBound
+            else
+                next
+        this
+    
