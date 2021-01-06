046388e46ec5af3aac7c28b3e002611c2f8b7e5e
Patrick Drechsler <patrick@pdrechsler.de>
2021-01-06 22:00:57 +0100

    ch12: case study 'Inappropriate Collection Types' - baseline

--- stylishFsharp/stylishFsharp.Console/Chapter12.fs
+++ stylishFsharp/stylishFsharp.Console/Chapter12.fs
@@ -3,21 +3,43 @@ module stylishFsharp.Console.Chapter12
 open System
 open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Running
-open Chapter12
 
+module InappropriateCollectionType =
+    module Old =
+        let sample interval data =
+            [
+                let max = (List.length data) - 1
+                for i in 0..interval..max ->
+                    data.[i]
+            ]
+
+    module New =
+        let sample interval data =
+            [
+                let max = (List.length data) - 1
+                for i in 0..interval..max ->
+                    data.[i]
+            ]
 
 module Harness =
     
     [<MemoryDiagnoser>]
     type Harness() =
-    
+
+        let r = Random()
+        let list = List.init 1_000_000 (fun _ -> r.NextDouble())
+        
         [<Benchmark>]
         member __.Old() =
-            Dummy.slowFunction()
+            list
+            |> InappropriateCollectionType.Old.sample 1000
+            |> ignore
         
         [<Benchmark>]
         member __.New() =
-            Dummy.fastFunction()
+            list
+            |> InappropriateCollectionType.Old.sample 1000
+            |> ignore
 
 let runChapter12 () =
     BenchmarkRunner.Run<Harness.Harness>()
