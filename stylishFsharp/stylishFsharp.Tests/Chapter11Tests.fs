3854a7b4503618618a3ff3107017849df271bea3
Patrick Drechsler <patrick@pdrechsler.de>
2021-01-06 18:16:57 +0100

    ch11: exercise 11-2 fix test filenames

--- stylishFsharp/stylishFsharp.Tests/Chapter11Tests.fs
+++ stylishFsharp/stylishFsharp.Tests/Chapter11Tests.fs
@@ -28,13 +28,13 @@ let ``Exercise 11-2 - Writing a ROP Pipeline`` () =
     
     let example =
         [|
-            { FileName = "2019-02-23T02:00:00:00-05:00"
+            { FileName = "2019-02-23T02:00:00-05:00"
               Content = [|1.0; 2.0; 3.0; 4.0|] }
-            { FileName = "2019-02-23T02:00:00:10-05:00"
+            { FileName = "2019-02-23T02:00:10-05:00"
               Content = [|5.0; 6.0; 7.0; 8.0|] }
             { FileName = "error"
               Content = [||] }
-            { FileName = "2019-02-23T02:00:00:20-05:00"
+            { FileName = "2019-02-23T02:00:20-05:00"
               Content = [|1.0; 2.0; 3.0; Double.NaN|] }
         |]
 
