4fb6b35ae207268bd4073de2310149da20d46e0d
Patrick Drechsler <patrick@pdrechsler.de>
2021-01-06 17:43:24 +0100

    ch11: exercise 11-2 setup

--- stylishFsharp/stylishFsharp.Tests/Chapter11Tests.fs
+++ stylishFsharp/stylishFsharp.Tests/Chapter11Tests.fs
@@ -3,7 +3,7 @@ module Chapter11Tests
 open Swensen.Unquote
 open Xunit
 
-open Chapter11
+open Chapter11.Exercise11_01
 
 let isFailure = function
     | Success _ -> 1 <>! 1
@@ -19,3 +19,26 @@ let ``Exercise 11-1 - Reproducing MapError`` () =
     
     // the same using built-in "mapError":
     "foo" |> Error |> Result.mapError id |> isError
+
+open System
+open Chapter11.Exercise11_02
+
+[<Fact>]
+let ``Exercise 11-2 - Writing a ROP Pipeline`` () =
+    
+    let example =
+        [|
+            { FileName = "2019-02-23T02:00:00:00-05:00"
+              Content = [|1.0; 2.0; 3.0; 4.0|] }
+            { FileName = "2019-02-23T02:00:00:10-05:00"
+              Content = [|5.0; 6.0; 7.0; 8.0|] }
+            { FileName = "error"
+              Content = [||] }
+            { FileName = "2019-02-23T02:00:00:20-05:00"
+              Content = [|1.0; 2.0; 3.0; Double.NaN|] }
+        |]
+
+//    example
+//    |> processData
+//    |> Array.iter (printfn "%A")
+    0
