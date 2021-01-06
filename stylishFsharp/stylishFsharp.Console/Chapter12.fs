ad0b6393c1e843ddbc4d19a2108a0fec0237a4f9
Patrick Drechsler <patrick@pdrechsler.de>
2021-01-06 22:19:17 +0100

    ch12: case study 'Inappropriate Collection Types' - using arrays instead of lists

--- stylishFsharp/stylishFsharp.Console/Chapter12.fs
+++ stylishFsharp/stylishFsharp.Console/Chapter12.fs
@@ -25,21 +25,39 @@ module InappropriateCollectionType =
         //            ]
 
         /// Listing 12-8
+        //        let sample interval data =
+        //            data
+        //            |> List.indexed
+        //            |> List.filter (fun (i, _) ->
+        //                i % interval = 0)
+        //            |> List.map snd
+        //        (*
+        //            | Method |       Mean |    Error |   StdDev |     Gen 0 |     Gen 1 |     Gen 2 |   Allocated |
+        //            |------- |-----------:|---------:|---------:|----------:|----------:|----------:|------------:|
+        //            |    Old | 1,325.1 ms | 24.79 ms | 21.97 ms |         - |         - |         - |    33.91 KB |
+        //            |    New |   135.1 ms |  2.67 ms |  6.75 ms | 8750.0000 | 5000.0000 | 1250.0000 | 62562.99 KB |
+        //        
+        //            - Pros: 10x faster
+        //            - Cons: a lot of garbage collection going on (we have 3 lists now!)
+        //        *)
+
+        /// Listing 12-10                
         let sample interval data =
             data
-            |> List.indexed
-            |> List.filter (fun (i, _) ->
+            |> Array.indexed
+            |> Array.filter (fun (i, _) ->
                 i % interval = 0)
-            |> List.map snd
+            |> Array.map snd
         (*
-            | Method |       Mean |    Error |   StdDev |     Gen 0 |     Gen 1 |     Gen 2 |   Allocated |
-            |------- |-----------:|---------:|---------:|----------:|----------:|----------:|------------:|
-            |    Old | 1,325.1 ms | 24.79 ms | 21.97 ms |         - |         - |         - |    33.91 KB |
-            |    New |   135.1 ms |  2.67 ms |  6.75 ms | 8750.0000 | 5000.0000 | 1250.0000 | 62562.99 KB |
-        
-            - Pros: 10x faster
-            - Cons: a lot of garbage collection going on (we have 3 lists now!)
+            | Method |        Mean |     Error |    StdDev |     Gen 0 |     Gen 1 |    Gen 2 |   Allocated |
+            |------- |------------:|----------:|----------:|----------:|----------:|---------:|------------:|
+            |    Old | 1,320.65 ms | 13.895 ms | 12.997 ms |         - |         - |        - |    33.91 KB |
+            |    New |    73.67 ms |  1.065 ms |  0.996 ms | 4428.5714 | 2571.4286 | 714.2857 | 39201.71 KB |
+            
+            - Using Array instead of List
+            - same Pros and Cons as before
         *)
+            
 module Harness =
     
     [<MemoryDiagnoser>]
@@ -47,6 +65,7 @@ module Harness =
 
         let r = Random()
         let list = List.init 1_000_000 (fun _ -> r.NextDouble())
+        let array = list |> Array.ofList
         
         [<Benchmark>]
         member __.Old() =
@@ -56,7 +75,7 @@ module Harness =
         
         [<Benchmark>]
         member __.New() =
-            list
+            array
             |> InappropriateCollectionType.New.sample 1000
             |> ignore
 
